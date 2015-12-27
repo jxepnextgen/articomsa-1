d62f70af29c2b65b299a97de312125dba3e36ae0
JoseEchegoyen <jx.echegoyen@gmail.com>
2015-12-26 19:39:23 -0600

    arregle el clavo de la vista

--- articomsa/js/LoadTable.js
+++ articomsa/js/LoadTable.js
@@ -4,11 +4,11 @@ $(document).ready(function (){
 var orden = document.getElementById('orden').value; 
 console.log (orden) ;
 
-	$.post ('../procesos/busquedas/BusquedaTicket.php', {orden:orden} ,
+	$.post ('../../procesos/busquedas/BusquedaTicket.php', {orden:orden} ,
 	function (data){ 	 
 console.log (data); 
 
-	var tabla;
+	var tabla ="";
 		var arreglo= jQuery.parseJSON (data);
 		console.log (arreglo);
 		for (i=0; i< arreglo.length ; i++){
